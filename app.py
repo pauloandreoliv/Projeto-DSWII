3a4f8fc9e4cdbf720ecb3c378e943c1358507b4c
Paulo André <pauloandrejanga@gmail.com>
2023-11-18 22:06:07 -0300

    Inserindo rotas

--- app.py
+++ app.py
@@ -3,8 +3,52 @@ from funcoes import obtersuporte_email
 
 app = Flask(__name__)
 
-@app.route('/obtersuporte', methods=['GET', 'POST'])
-def obter_suporte():
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/principal')
+def principal():
+    return render_template('principal.html')
+
+@app.route('/cadastrar')
+def cadastrar():
+    return render_template('cadastrar.html')
+
+@app.route('/esqueceu')
+def esqueceu():
+    return render_template('esqueceu.html')
+
+@app.route('/minhaslistas')
+def minhaslistas():
+    return render_template('minhaslistas.html')
+
+@app.route('/criarlista')
+def criarlista():
+    return render_template('criarlista.html')
+
+@app.route('/verlista')
+def verlista():
+    return render_template('verlista.html')
+
+@app.route('/verprodutos')
+def verprodutos():
+    return render_template('verprodutos.html')
+
+@app.route('/criarproduto')
+def cadastrarproduto():
+    return render_template('criarproduto.html')
+
+@app.route('/verdicas')
+def verdicas():
+    return render_template('verdicas.html')
+
+@app.route('/verpromocoes')
+def verpromocoes():
+    return render_template('verpromocoes.html')
+
+@app.route('/obtersuporte', methods=['GET','POST'])
+def obtersuporte():
     if request.method == 'POST':
         email = request.form['email']
         texto = request.form['texto']
