27952e6f70e158ac663ea36efe51fd5a8ed50fcb
Paulo André <pauloandrejanga@gmail.com>
2023-12-07 21:01:33 -0300

    Inserindo funcionalidades e conexão com front-end

--- api/app.py
+++ api/app.py
@@ -16,20 +16,40 @@ logger.addHandler(fileHandler)
 app = Flask(__name__)
 app.config.from_object('config')
 
-cred = credentials.Certificate('api/key.json') 
+cred = credentials.Certificate({
+  "type": "service_account",
+  "project_id": "app-mercado-4bae7",
+  "private_key_id": "da5dc5afdf92b6008fdfec25c37c094c7e2a95db",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDJKWL2nLXUvOLz\nn2AaMI6IAEyB+CZD1YNV0QyovDRFH9VYgXFbDkOH7x844i/1H762skkvk7PrKmvF\nwWqK/FBKgbIDbqVxBU6L8bII/ybkz9zXCtawsWXMETyoQqYyEyxRh+iqCLzeIXeO\njLrXoA5d+FpVsThx8uhdE1czrTW6bjhK1hBgvL7ELFkoZ7TowbQnW9wLDK8WG6/Z\nngIa04uDw1/rRQ20J+FmXTA+0TtzjGIhGFAMP4X8ebFqdfVYIYbOM3u+/+n2/hVy\n4Qxw6mcs5uhvBMo8/kANv5lfc64ovknIiWI938SDDXKJoCZT4IyxAZ1fyPz3x9vA\nX7Qdrt5RAgMBAAECggEAFsWroVVOND4JxyCEpZJXoEfAYXoxAr7Z9k4LC9L6a9TV\ntwY4SeGMfYfxVeQtH6evVElMhalH1dhgiOuDXyZ/BGA4QbE141yBbtA2olQYGBD7\n5BAjC6+ZYYeWAUpCPCr1emlTSmRg1RieyzFdOWmyFqs1truwMx4Xdenplj0Gdwst\neBcPlWIbgfk3P8+mczbKY9YOFpnGwFWonLwA2SnWzoEO9r9csO/lW+gIk5ZKcI7j\nRRDjmW2/OH09CfdORH1xKEt5uRbjpPun4fdi3Ijh0/j2bM9CtiL0T5+NoztunXEW\nzDJD31cMbgcKwhGU/AYYA6izwBzgtahrl85zvvzY0QKBgQD+PWviKKlOrhxnZWzX\nAdJh2Jn45n6E+CeZxc20vfsoAi1njalWoxly61R1qs3bkWSMXKMK3Xb+7Oaw6WA5\nC2wDmBTN7atAxQnK5N6mdmrJCE0Wi/KgPA3QoB8hisTdjO5K7t4NyolxzQpVVexg\n0sUWmrR7ddZjwoX8euxKWGs1RQKBgQDKjeWV00KTqLjpngcJ1TmXkvXbHWH8FQlp\nq/+W/iMYX/J28LsICri1XNzAY2YYZGeJ/mw062GS42QFZaQHvJ873LFl2mtOWGIy\njTTaIODGLMk3py3sZZH6FoD9V4b3Oaczfx+UBjKMXi2ZLZ4QvbJ1TVBK+31RlvB2\nvqs5jO4XnQKBgAMzUL1bF66G4JKoozMdscFLkbyhYixYV19HLyy8S4IYbOvly6Ds\nkrBwp/KHQOonrWVa8S581TemUc0eUvjE3Qo6iuCAwmwhTttAAFGGIhf/w8Cp6Fb+\nCop6yYmJjqrfUEyfLyprR0modB3Y5A3f+V0se2Mme5lr5HXmMj1JWaglAoGAOikY\nXTb2DPQLdHk9yCMDkHRQSARd2EYqF7+dejuq1BrowiJJcfuyIdFySYPBEwvxdvwp\nEeDseYL1e18+BEKy7jtmbLqUyUAzohRNsrxyDjVZ6bMFSrW3frnre9MgG+jgC7da\n1+QXq5p3OV6R3ni5v1jdz0n7J+xRS6df/vxqeTkCgYBVUwRvR1RJGa9gPwx7W3ti\nF8AY5Nd3fDKjG9U0VutIYjESLqMoQUMqZixbWgcVgRC6g4Yd+Q/jaBLAYGTl/3al\npTwGfGp7iUSR4oOkJ/8FmX+iJkOJf3R6QKYVgUxe1RUAhR9u0FHA7NN1DP2i4Xg4\nOeIqxYHMlCZg3wNsypcsPw==\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-xi17o@app-mercado-4bae7.iam.gserviceaccount.com",
+  "client_id": "100181592438930488100",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-xi17o%40app-mercado-4bae7.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}) 
 default_app = initialize_app(cred) 
 db = firestore.client() 
 user_ref = db.collection('user')
 lista_ref = db.collection('lista')
 produto_ref = db.collection('produto')
+promocao_ref = db.collection('promocao')
+dica_ref = db.collection('dica')
 
 @app.route('/user', methods=['POST'])
 def create_user():
     try:
         id = request.json['id']
-        user_ref.document(str(id)).set(request.json)
-        logger.info(f'Usuário criado com id={id}')
-        return jsonify({"success": True}), 200
+        userid_ref = user_ref.document(id)
+        dados = userid_ref.get().to_dict()
+        if dados and dados.get('id') == id:
+            logger.info(f"{id} já cadastrado")
+            return jsonify({"error": "CPF já cadastrado"}), 401
+        else:
+            user_ref.document(str(id)).set(request.json)
+            logger.info(f'Usuário criado com id={id}')
+            return jsonify({"success": True}), 200
     except Exception as e:
         logger.error(f"An Error Occurred: {e}")
         return f"An Error Occurred: {e}", 500
@@ -75,7 +95,7 @@ def get_listas_por_creator():
         for lista in todas_listas:
             dados = lista.to_dict()
             if 'id_creator' in dados and dados['id_creator'] == id_creator:
-                listas_por_creator.append({"id": lista.nome, "id": lista.nome, "lista": dados})
+                listas_por_creator.append({"id": dados['id'], "nome": dados['nome']})
         if listas_por_creator:
             logger.info(f'Listas retornadas com sucesso para o ID creator {id_creator}')
             return jsonify({"listas": listas_por_creator}), 200
@@ -84,7 +104,6 @@ def get_listas_por_creator():
     except Exception as e:
         logger.error(f"An Error Occurred: {e}")
         return f"An Error Occurred: {e}", 500
-
 
 @app.route('/lista', methods=['POST'])
 def create_lista():
@@ -113,7 +132,7 @@ def delete_lista():
 @app.route('/produto', methods=['GET'])
 def get_produtos():
     try:
-        all_id = [doc.to_dict() for doc in user_ref.stream()]
+        all_id = [doc.to_dict() for doc in produto_ref.stream()]
         logger.warning('all firebase data returned')
         return jsonify(all_id), 200
     except Exception as e:
@@ -140,6 +159,26 @@ def delete_produto():
         lista_ref.document(id).delete()
         logger.info(f'Produto com id={id} deletado')
         return jsonify({"success": True}), 200
+    except Exception as e:
+        logger.error(f"An Error Occurred: {e}")
+        return f"An Error Occurred: {e}", 500
+
+@app.route('/promocoes', methods=['GET'])
+def get_promocoes():
+    try:
+        all_id = [doc.to_dict() for doc in promocao_ref.stream()]
+        logger.warning('all firebase data returned')
+        return jsonify(all_id), 200
+    except Exception as e:
+        logger.error(f"An Error Occurred: {e}")
+        return f"An Error Occurred: {e}", 500
+
+@app.route('/dicas', methods=['GET'])
+def get_dicas():
+    try:
+        all_id = [doc.to_dict() for doc in dica_ref.stream()]
+        logger.warning('all firebase data returned')
+        return jsonify(all_id), 200
     except Exception as e:
         logger.error(f"An Error Occurred: {e}")
         return f"An Error Occurred: {e}", 500
---
+++ app.py
@@ -0,0 +1,176 @@
+from flask import Flask, session, redirect, url_for, render_template, request
+from funcoes import obtersuporte_email
+import requests
+import json
+import logging
+
+logging.basicConfig(filename='logs/debug.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+app = Flask(__name__)
+app.secret_key = 'aplicacaoWeb2023.2'
+
+url_api = "https://api-dswii.onrender.com"
+
+#Renderização das páginas HTML
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/principal')
+def principal():
+    if session.get('logado') != None:
+        return render_template('principal.html')
+    else:
+        return render_template('erro.html')
+
+@app.route('/cadastrar')
+def cadastrar():
+    return render_template('cadastrar.html')
+
+@app.route('/esqueceu')
+def esqueceu():
+    return render_template('esqueceu.html')
+
+@app.route('/minhaslistas')
+def minhaslistas():
+    if session.get('logado') != None:
+        id_creator = session.get('logado')['id']
+        data = {"id_creator": id_creator}
+        return_request = requests.get(f"{url_api}/listas_por_creator", json=data)
+        if return_request.status_code == 200:
+            itens = json.loads(return_request.text)
+            return render_template("minhaslistas.html", itens=itens)
+        else:
+            return render_template('minhaslistas.html', itens={})
+    else:
+        return render_template('erro.html')
+
+@app.route('/criarlista')
+def criarlista():
+    if session.get('logado') != None:
+        return render_template('criarlista.html')
+    else:
+        return render_template('erro.html')
+
+@app.route('/verlista', methods=['GET'])
+def verlista():
+    if session.get('logado') != None:
+        id = request.args.get('id')
+        return_request = requests.get(f"{url_api}/lista", json={"id": id})
+        if return_request.status_code == 200:
+            itens = json.loads(return_request.text)
+            return render_template('verlista.html', itens=itens)
+        else:
+            return render_template('verlista.html', itens={"qtd": 0, "nome": "Sem produtos", "preco": 0})
+    else:
+        return render_template('erro.html')
+
+@app.route('/verprodutos')
+def verprodutos():
+    if session.get('logado') != None:
+        return_request = requests.get(f"{url_api}/produto")
+        if return_request.status_code == 200:
+            itens = json.loads(return_request.text)
+            return render_template("verprodutos.html", itens=itens)
+        else:
+            return render_template('verprodutos.html', itens={"nome": "Sem produtos", "preco": 0})
+    else:
+        return render_template('erro.html')
+
+@app.route('/criarproduto')
+def criarproduto():
+    if session.get('logado') != None:
+        return render_template('criarproduto.html')
+    else:
+        return render_template('erro.html')
+
+@app.route('/verpromocoes')
+def verpromocoes():
+    if session.get('logado') != None:
+        return_request = requests.get(f"{url_api}/promocoes")
+        if return_request.status_code == 200:
+            itens = json.loads(return_request.text)
+            return render_template("verpromocoes.html", itens=itens)
+        else:
+            return render_template('verpromocoes.html', itens={"nome": "Sem promoções", "preco": 0})
+    else:
+        return render_template('erro.html')
+
+@app.route('/verdicas')
+def verdicas():
+    if session.get('logado') != None:
+        return_request = requests.get(f"{url_api}/dicas")
+        if return_request.status_code == 200:
+            itens = json.loads(return_request.text)
+            return render_template("verdicas.html", itens=itens)
+        else:
+            return render_template('verdicas.html', itens={"dica": "Sem dicas"})
+    else:
+        return render_template('erro.html')
+
+#Funcionalidades
+@app.route('/obtersuporte', methods=['GET','POST'])
+def obtersuporte():
+    if request.method == 'POST':
+        email = request.form['email']
+        texto = request.form['texto']
+
+        aviso= obtersuporte_email(email, texto) if email != '' and texto != '' else 'Preencha os campos'
+        return render_template('obtersuporte.html', aviso=aviso)
+
+    return render_template('obtersuporte.html', aviso='')
+
+@app.route('/login', methods=['POST'])
+def login():
+    session.pop('logado', None)
+    data = {
+        "id" : str(request.form["cpf"]),
+        "senha" : str(request.form["senha"])
+    }
+    return_request = requests.get(f"{url_api}/login", json=data)
+    if return_request.status_code == 200:
+        id = return_request.json()
+        session['logado'] = id
+        logging.info(f'Sessão de usuário {id} iniciada')
+        return 'Login realizado com sucesso. <a href="/principal">Página Inicial</a>'
+    else:
+        logging.info(f'Sessão de usuário {id} não permitida')
+        return f"{return_request.status_code} : {return_request.text}  <br> <a href='/entrar'>Tentar novamente</a>"
+
+@app.route('/cadastrarusuario', methods=["POST"])
+def cadastrarusuario():
+    data = {
+        "id" : str(request.form["cpf"]),
+        "senha" : str(request.form["senha"])
+    }
+    return_request = requests.post(f"{url_api}/user", json=data)
+
+    if return_request.status_code == 200:
+        logging.info(f'Cadastro realizado com sucesso: {id}')
+        return 'Cadastro realizado com sucesso. <a href="/">Página Inicial</a>'
+    else:
+        logging.info(f'Não foi possível cadastrar id: {id} -> {return_request.text}')
+        return f"{return_request.status_code} : {return_request.text}  <br> <a href='/cadastrar'>Tentar novamente</a>"
+        
+@app.route('/cadastrarproduto', methods=["POST"])
+def cadastrarproduto():
+    data = {
+        "nome" : str(request.form["nomedoproduto"]),
+        "preco" : str(request.form["precodoproduto"])
+    }
+    return_request = requests.post(f"{url_api}/produto", json=data)
+
+    if return_request.status_code == 200:
+        logging.info(f'Cadastro realizado com sucesso: {id}')
+        return 'Cadastro realizado com sucesso. <a href="/principal">Página Inicial</a>'
+    else:
+        logging.info(f'Não foi possível cadastrar id: {id} -> {return_request.text}')
+        return f"{return_request.status_code} : {return_request.text}  <br> <a href='/criarproduto'>Tentar novamente</a>"
+        
+@app.route('/logout')
+def logout():
+    session.pop('logado', None)
+    return 'Logout realizado com sucesso. <a href="/">Página Inicial</a>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
